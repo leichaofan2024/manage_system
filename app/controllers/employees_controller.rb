33186f59077d2eb8c0a3b9a11a802f47a050d4a3
13949836783@163.com <13949836783@163.com>
2018-04-18 11:59:29 +0800

    完成年龄分析饼图基本制作

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -20,7 +20,11 @@ class EmployeesController < ApplicationController
   	
   end
 
+ ### 统计分析页面的图表数据配置---开始
   def statistical_analysis
+    # 年龄分析-饼图数据配置---开始
+
+    ## 把各个年龄段的人数捞出，赋值给对应的实例变量
   	@age_25_below = Employee.where(age: 0..25).count
     @age_25 = Employee.where(age: 25..30).count
     @age_30 = Employee.where(age: 30..35).count
@@ -29,29 +33,21 @@ class EmployeesController < ApplicationController
     @age_45 = Employee.where(age: 45..50).count
     @age_50 = Employee.where(age: 50..55).count
     @age_55 = Employee.where(age: 55..60).count
-
-    age_count = {}
-    age_count = {
-      "25岁以下" => @age_25_below, 
-      "25-30岁" => @age_25, 
-      "30-35岁" => @age_30, 
-      "35-40岁" => @age_35, 
-      "40-45岁" => @age_40, 
-      "45-50岁" => @age_45, 
-      "50-55岁" => @age_50, 
-      "55-60岁" => @age_55
-    }
     
-    gon.twenty_five_below = {name: "25岁以下", value: age_count["25岁以下"]}
-    gon.twenty_five = {name: "25-30岁", value: age_count["25-30岁"]}
-    gon.thirty = {name: "30-35岁", value: age_count["30-35岁"]}
-    gon.thirty_five = {name: "35-40岁", value: age_count["35-40岁"]}
-    gon.forty = {name: "40-45岁", value: age_count["40-45岁"]}
-    gon.forty_five = {name: "45-50岁", value: age_count["45-50岁"]}
-    gon.fifty = {name: "50-55岁", value: age_count["50-55岁"]}
-    gon.fifty_five = {name: "55-60岁", value: age_count["55-60岁"]}
+    # 使用'gon'这个gem的方法，将数据以hash方式赋值给对应的变量
+    gon.twenty_five_below = @age_25_below
+    gon.twenty_five = @age_25
+    gon.thirty =  @age_30
+    gon.thirty_five =  @age_35
+    gon.forty =  @age_40
+    gon.forty_five = @age_45
+    gon.fifty = @age_50
+    gon.fifty_five = @age_55
+   
+    ## 年龄分析-饼图数据配置---结束
   
   end
+  ### 统计分析页面的图表数据配置---结束
 
   def import
     Employee.import(params[:file])
