33d3a78bd67595b1596439566be66d75f9ea173f
13949836783@163.com <13949836783@163.com>
2018-04-19 10:46:37 +0800

    完成柱状图例子

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,48 +1,28 @@
 class EmployeesController < ApplicationController
-  layout 'home'
+
   def index
-    @employees = Employee.page(params[:page])
-    @export_employees = Employee.order("id ASC")
-    respond_to do |format|
-      format.html
-      format.csv { send_data @employees.to_csv }
-      format.xls
-    end
-  end
-
-  def import_table
-    Employee.import_table(params[:file])
-    redirect_to employees_path
-  end
-
-  def search
-    @employees = Employee.search(params[:q]).page(params[:page]).records
-    render action: "index"
-  end
-
-  def update_sal_number
-    @employees = Employee.all
-    @employees.each do |employee|
-      employee.sal_number = '41' + employee.job_number
-      employee.save!
-    end
-    redirect_to employees_path
+    @employees = Employee.page(params[:page]).per(20)
   end
 
   def new
-
+  	
   end
 
   def edit
-
+  	
   end
 
   def show
+  	
+  end
 
+  def import
+    Employee.import(params[:file])
+    redirect_to employees_path
   end
 
   def organization_structure
-
+  	
   end
 
 ### 统计分析页面的图表数据配置---开始
@@ -56,7 +36,7 @@ class EmployeesController < ApplicationController
     @age_40 = Employee.where(age: 40..45).count
     @age_45 = Employee.where(age: 45..50).count
     @age_50 = Employee.where(age: 50..55).count
-    @age_55 = Employee.where(age: 55..60).count
+    @age_55 = Employee.where(age: 55..60).count    
     # 使用'gon'这个gem的方法，将数据赋值给对应的变量
     gon.twenty_five_below = @age_25_below
     gon.twenty_five = @age_25
@@ -65,7 +45,7 @@ class EmployeesController < ApplicationController
     gon.forty =  @age_40
     gon.forty_five = @age_45
     gon.fifty = @age_50
-    gon.fifty_five = @age_55
+    gon.fifty_five = @age_55   
   ## 年龄分析-饼图数据配置---结束
 
   ## 学历分析-饼图数据配置---开始
@@ -86,6 +66,26 @@ class EmployeesController < ApplicationController
     gon.undergraduate = @undergraduate
     gon.postgraduate = @postgraduate
   ## 学历分析-饼图数据配置---结束
+
+  ## 年龄分析-柱状图数据配置---开始
+    # 把车间各个学历的人数捞出，赋值给对应的实例变量
+    @junior_high_school_below = Employee.where(education_background: ["小学", "初中"]).count
+    @senior_high_school = Employee.where(education_background: "高中").count
+    @technical_school = Employee.where(education_background: "技校").count
+    @secondary_school = Employee.where(education_background: "中专").count
+    @university_specialties = Employee.where(education_background: "大学专科").count
+    @undergraduate = Employee.where(education_background: "大学本科").count
+    @postgraduate = Employee.where(education_background: "研究生").count
+    # 使用'gon'这个gem的方法，将数据赋值给对应的变量
+    gon.junior_high_school_below = @junior_high_school_below
+    gon.senior_high_school = @senior_high_school
+    gon.technical_school =  @technical_school
+    gon.secondary_school =  @secondary_school
+    gon.university_specialties =  @university_specialties
+    gon.undergraduate = @undergraduate
+    gon.postgraduate = @postgraduate
+  ## 年龄分析-柱状图数据配置---结束
+
   end
 ### 统计分析页面的图表数据配置---结束
 
