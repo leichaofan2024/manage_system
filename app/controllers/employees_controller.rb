0653d66466b2901d71d8a7163fdb65f994752d45
kafeidebai <zhanggongzifei@163.com>
2018-04-20 16:06:04 +0800

    分页翻译

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,7 +1,7 @@
 class EmployeesController < ApplicationController
   layout 'home'
   def index
-    @employees = Employee.page(params[:page]).per(15)
+    @employees = Employee.page(params[:page]).per(20)
     @export_employees = Employee.order("id ASC")
     respond_to do |format|
       format.html
