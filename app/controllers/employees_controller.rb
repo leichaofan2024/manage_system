6445cd2fc0677073259717db502ffd6b571f76e2
kafeidebai <zhanggongzifei@163.com>
2018-04-25 00:15:31 +0800

    增加横向导航栏

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,7 +1,7 @@
 class EmployeesController < ApplicationController
   layout 'home'
   def index
-    @employees = Employee.page(params[:page]).per(20)
+    @employees = Employee.page(params[:page]).per(50)
     @export_employees = Employee.order("id ASC")
     respond_to do |format|
       format.html
