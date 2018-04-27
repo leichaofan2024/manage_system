88defabcc5bec5f0e80a03658a612b3ebad6aa3b
13949836783@163.com <13949836783@163.com>
2018-04-27 20:23:35 +0800

    完成注释

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -54,8 +54,10 @@ class EmployeesController < ApplicationController
 
   end
 
+###年龄、工龄、路龄、学历图表分析页面数据配置---开始
+  ##年龄分析
   def age_statistical_analysis
-    ## 年龄分析-饼图数据配置---开始
+    #年龄分析---饼图数据设置---开始
     # 把各个年龄段的人数捞出，赋值给对应的实例变量
     @age_25_below = Employee.where(age: 0..25).count
     @age_25 = Employee.where(age: 25..30).count
@@ -74,94 +76,91 @@ class EmployeesController < ApplicationController
     gon.forty_five = @age_45
     gon.fifty = @age_50
     gon.fifty_five = @age_55
-  ## 年龄分析-饼图数据配置---结束
+    #年龄分析---饼图数据设置---结束
+    #年龄分析---条形图数据设置---开始
+    @workshops = Employee.pluck("workshop").uniq
+      #定义每个年龄段各个车间的hash（包括在循环里使用的和最后存入的）
+      loop_hash_25_below = {}
+      hash_25_below = {}
+      loop_hash_25 = {}
+      hash_25 = {}
+      loop_hash_30 = {}
+      hash_30 = {}
+      loop_hash_35 = {}
+      hash_35 = {}
+      loop_hash_40 = {}
+      hash_40 = {}
+      loop_hash_45 = {}
+      hash_45 = {}
+      loop_hash_50 = {}
+      hash_50 = {}
+      loop_hash_55 = {}
+      hash_55 = {}
+      #使用循环把车间和人数存成hash
+      @workshops.each do |i|
+        #把每个车间的总人数存成变量
+        emp = Employee.where(workshop: i).count
+        #把各年龄段在各车间的人数存成变量
+        a1 = Employee.where(workshop: i, age: 0...25).count      
+        #算出各年龄段人数占车间总人数的比重
+        a = (a1.to_f)/(emp.to_f)      
+        #将每一次的计算结果存成"车间 => 百分比"的hash
+        loop_hash_25_below = {i => a}     
+        #将每一次的hash结果存到最终的hash里，供之后使用
+        hash_25_below[i] = loop_hash_25_below[i]
 
-  @workshops = Employee.pluck("workshop").uniq
-  #定义每个年龄段各个车间的hash（包括在循环里使用的和最后存入的）
-    loop_hash_25_below = {}
-    hash_25_below = {}
-    loop_hash_25 = {}
-    hash_25 = {}
-    loop_hash_30 = {}
-    hash_30 = {}
-    loop_hash_35 = {}
-    hash_35 = {}
-    loop_hash_40 = {}
-    hash_40 = {}
-    loop_hash_45 = {}
-    hash_45 = {}
-    loop_hash_50 = {}
-    hash_50 = {}
-    loop_hash_55 = {}
-    hash_55 = {}
-
-    #使用循环把车间和人数存成hash
-    @workshops.each do |i|
-      #把每个车间的总人数存成变量
-      emp = Employee.where(workshop: i).count
-      #年龄、学历、工龄、路龄在各车间的人数
-      a1 = Employee.where(workshop: i, age: 0...25).count      
-      #算出各维度人数占总人数的比重
-      a = (a1.to_f)/(emp.to_f)      
-      #将每一次的计算结果存成"车间 => 百分比"的hash
-      loop_hash_25_below = {i => a}     
-      #将每一次的hash结果存到最终的结果里，供之后使用
-      hash_25_below[i] = loop_hash_25_below[i]
+        b1 = Employee.where(workshop: i, age: 25...30).count      
+        b = (b1.to_f)/(emp.to_f)     
+        loop_hash_25 = {i => b}      
+        hash_25[i] = loop_hash_25[i]
+        
+        c1 = Employee.where(workshop: i, age: 30...35).count      
+        c = (c1.to_f)/(emp.to_f)     
+        loop_hash_30 = {i => c}      
+        hash_30[i] = loop_hash_30[i]
       
-      b1 = Employee.where(workshop: i, age: 25...30).count      
-      b = (b1.to_f)/(emp.to_f)     
-      loop_hash_25 = {i => b}      
-      hash_25[i] = loop_hash_25[i]
-      
-      c1 = Employee.where(workshop: i, age: 30...35).count      
-      c = (c1.to_f)/(emp.to_f)     
-      loop_hash_30 = {i => c}      
-      hash_30[i] = loop_hash_30[i]
-    
-      d1 = Employee.where(workshop: i, age: 35...40).count     
-      d = (d1.to_f)/(emp.to_f)    
-      loop_hash_35 = {i => d}     
-      hash_35[i] = loop_hash_35[i]    
+        d1 = Employee.where(workshop: i, age: 35...40).count     
+        d = (d1.to_f)/(emp.to_f)    
+        loop_hash_35 = {i => d}     
+        hash_35[i] = loop_hash_35[i]    
 
-      e1 = Employee.where(workshop: i, age: 40...45).count     
-      e = (e1.to_f)/(emp.to_f)    
-      loop_hash_40 = {i => e}     
-      hash_40[i] = loop_hash_40[i]
-     
-      f1 = Employee.where(workshop: i, age: 45...50).count      
-      f = (f1.to_f)/(emp.to_f)     
-      loop_hash_45 = {i => f}      
-      hash_45[i] = loop_hash_45[i]
-      
-      g1 = Employee.where(workshop: i, age: 50...55).count     
-      g = (g1.to_f)/(emp.to_f)    
-      loop_hash_50 = {i => g}    
-      hash_50[i] = loop_hash_50[i]
-      
-      h1 = Employee.where(workshop: i, age: 55..70).count     
-      h = (h1.to_f)/(emp.to_f)    
-      loop_hash_55 = {i => h}     
-      hash_55[i] = loop_hash_55[i]
+        e1 = Employee.where(workshop: i, age: 40...45).count     
+        e = (e1.to_f)/(emp.to_f)    
+        loop_hash_40 = {i => e}     
+        hash_40[i] = loop_hash_40[i]
+       
+        f1 = Employee.where(workshop: i, age: 45...50).count      
+        f = (f1.to_f)/(emp.to_f)     
+        loop_hash_45 = {i => f}      
+        hash_45[i] = loop_hash_45[i]
+        
+        g1 = Employee.where(workshop: i, age: 50...55).count     
+        g = (g1.to_f)/(emp.to_f)    
+        loop_hash_50 = {i => g}    
+        hash_50[i] = loop_hash_50[i]
+        
+        h1 = Employee.where(workshop: i, age: 55..70).count     
+        h = (h1.to_f)/(emp.to_f)    
+        loop_hash_55 = {i => h}     
+        hash_55[i] = loop_hash_55[i]
+      end
+      #生成每个年龄段的【车间-人数】hash---结束
 
-      gon.bar_workshop = hash_25_below.keys
-      #将以上算出最终hash的values赋值给对应的变量，作为条形图的数据
-      #年龄分析条形图
-      gon.bar_twenty_five_below = hash_25_below.values
-      gon.bar_twenty_five = hash_25.values
-      gon.bar_thirty = hash_30.values
-      gon.bar_thirty_five = hash_35.values
-      gon.bar_forty = hash_40.values
-      gon.bar_forty_five = hash_45.values
-      gon.bar_fifty = hash_50.values
-      gon.bar_fifty_five = hash_55.values
-
-    end
-    #生成每个年龄段的【车间-人数】hash---结束
+    #将以上算出最终hash的keys赋值给对应的变量，作为条形图的坐标轴信息
+    gon.bar_workshop = hash_25_below.keys
+    #将以上算出最终hash的values赋值给对应的变量，作为条形图的数据
+    gon.bar_twenty_five_below = hash_25_below.values
+    gon.bar_twenty_five = hash_25.values
+    gon.bar_thirty = hash_30.values
+    gon.bar_thirty_five = hash_35.values
+    gon.bar_forty = hash_40.values
+    gon.bar_forty_five = hash_45.values
+    gon.bar_fifty = hash_50.values
+    gon.bar_fifty_five = hash_55.values
+    #年龄分析---条形图数据设置---结束
   end
 
   def education_statistical_analysis
-    ## 学历分析-饼图数据配置---开始
-    # 把各个学历的人数捞出，赋值给对应的实例变量
     @junior_high_school = Employee.where(education_background: ["初中"]).count
     @primary_school = Employee.where(education_background: ["小学"]).count
     @senior_high_school = Employee.where(education_background: "高中").count
@@ -170,7 +169,7 @@ class EmployeesController < ApplicationController
     @university_specialties = Employee.where(education_background: "大学专科").count
     @undergraduate = Employee.where(education_background: "大学本科").count
     @postgraduate = Employee.where(education_background: "研究生").count
-    # 使用'gon'这个gem的方法，将数据赋值给对应的变量，在js中使用
+
     gon.junior_high_school = @junior_high_school_below
     gon.senior_high_school = @senior_high_school
     gon.technical_school =  @technical_school
@@ -179,12 +178,8 @@ class EmployeesController < ApplicationController
     gon.undergraduate = @undergraduate
     gon.postgraduate = @postgraduate
     gon.primary_school = @primary_school
-  ## 学历分析-饼图数据配置---结束
-  ## 条形图数据配置---开始
-    #捞出所有的车间，赋值给实例变量
+
     @workshops = Employee.pluck("workshop").uniq
-    #生成【车间-人数】hash---开始
-    #定义每种学历各个车间的hash（包括在循环里使用的和最后存入的）
     loop_hash_junior_high_school = {}
     hash_junior_high_school = {}
     loop_hash_primary_school = {}
@@ -201,14 +196,13 @@ class EmployeesController < ApplicationController
     hash_undergraduate = {}
     loop_hash_postgraduate = {}
     hash_postgraduate = {}
-     #使用循环把车间和人数存成hash
     @workshops.each do |i|
       emp = Employee.where(workshop: i).count     
       a1 = Employee.where(workshop: i, education_background: "初中").count
       a = (a1.to_f)/(emp.to_f)
       loop_hash_junior_high_school = {i => a}
       hash_junior_high_school[i] = loop_hash_junior_high_school[i]
-      
+
       b1 = Employee.where(workshop: i, education_background: "小学").count
       b = (b1.to_f)/(emp.to_f)
       loop_hash_primary_school = {i => b}
@@ -244,12 +238,7 @@ class EmployeesController < ApplicationController
       loop_hash_postgraduate = {i => h}
       hash_postgraduate[i] = loop_hash_postgraduate[i]
     end
-    #生成每个年龄段的【车间-人数】hash---结束
-     # 使用'gon'这个gem的方法来赋值变量，在js中使用
-    # 将以上算出最终hash的keys赋值给变量，作为条形图的坐标轴
     gon.bar_workshop = hash_junior_high_school.keys
-    #将以上算出最终hash的values赋值给对应的变量，作为条形图的数据
-    #学历分析条形图
     gon.bar_junior_high_school = hash_junior_high_school.values
     gon.bar_primary_school = hash_primary_school.values
     gon.bar_senior_high_school = hash_senior_high_school.values
@@ -261,8 +250,6 @@ class EmployeesController < ApplicationController
   end
 
   def working_years_statistical_analysis
-     ## 工龄分析-饼图数据配置---开始
-    # 把各个工龄段的人数捞出，赋值给对应的实例变量
     @working_years_5_below = Employee.where(working_years: 0...5).count
     @working_years_5 = Employee.where(working_years: 5...10).count
     @working_years_10 = Employee.where(working_years: 10...15).count
@@ -272,7 +259,7 @@ class EmployeesController < ApplicationController
     @working_years_30 = Employee.where(working_years: 30...35).count
     @working_years_35 = Employee.where(working_years: 35...40).count
     @working_years_40_up = Employee.where(working_years: 40..100).count
-    # 使用'gon'这个gem的方法，将数据赋值给对应的变量，在js中使用
+
     gon.working_years_5_below = @working_years_5_below
     gon.working_years_5 = @working_years_5
     gon.working_years_10 = @working_years_10
@@ -282,13 +269,8 @@ class EmployeesController < ApplicationController
     gon.working_years_30 = @working_years_30
     gon.working_years_35 = @working_years_35
     gon.working_years_40_up = @working_years_40_up
-  ## 工龄分析-饼图数据配置---结束
-
-  ## 条形图数据配置---开始
-    #捞出所有的车间，赋值给实例变量
+    
     @workshops = Employee.pluck("workshop").uniq
-    #生成【车间-人数】hash---开始
-    #定义每个工龄段各个车间的hash（包括在循环里使用的和最后存入的）
     loop_hash_working_5_below = {}
     hash_working_5_below = {}
     loop_hash_working_5 = {}
@@ -307,7 +289,7 @@ class EmployeesController < ApplicationController
     hash_working_35 = {}
     loop_hash_working_40_up = {}
     hash_working_40_up = {}
-    #使用循环把车间和人数存成hash
+
     @workshops.each do |i|
       emp = Employee.where(workshop: i).count
       a1 = Employee.where(workshop: i, working_years: 0...5).count
@@ -354,9 +336,8 @@ class EmployeesController < ApplicationController
       j = (j1.to_f)/(emp.to_f)
       loop_hash_working_40_up = {i => j}
       hash_working_40_up[i] = loop_hash_working_40_up[i]
+
       gon.bar_workshop = hash_working_5_below.keys
-      #将以上算出最终hash的values赋值给对应的变量，作为条形图的数据
-      #工龄分析条形图
       gon.bar_working_5_below = hash_working_5_below.values
       gon.bar_working_5 = hash_working_5.values
       gon.bar_working_10 = hash_working_10.values
@@ -367,15 +348,9 @@ class EmployeesController < ApplicationController
       gon.bar_working_35 = hash_working_35.values
       gon.bar_working_40_up = hash_working_40_up.values
     end
-    #生成每个年龄段的【车间-人数】hash---结束
-
   end
 
-
-
   def rali_years_statistical_analysis
-  ## 路龄分析-饼图数据配置---开始
-    # 把各个路龄段的人数捞出，赋值给对应的实例变量
     @rali_years_5_below = Employee.where(rali_years: 0...5).count
     @rali_years_5 = Employee.where(rali_years: 5...10).count
     @rali_years_10 = Employee.where(rali_years: 10...15).count
@@ -384,7 +359,7 @@ class EmployeesController < ApplicationController
     @rali_years_25 = Employee.where(rali_years: 25...30).count
     @rali_years_30 = Employee.where(rali_years: 30...35).count
     @rali_years_35_up = Employee.where(rali_years: 35..100).count
-    # 使用'gon'这个gem的方法，将数据赋值给对应的变量，在js中使用
+
     gon.rali_years_5_below = @rali_years_5_below
     gon.rali_years_5 = @rali_years_5
     gon.rali_years_10 = @rali_years_10
@@ -393,10 +368,8 @@ class EmployeesController < ApplicationController
     gon.rali_years_25 = @rali_years_25
     gon.rali_years_30 = @rali_years_30
     gon.rali_years_35_up = @rali_years_35_up
-  ## 路龄分析-饼图数据配置---结束
 
   @workshops = Employee.pluck("workshop").uniq
-    #定义每个路龄段各个车间的hash（包括在循环里使用的和最后存入的）
     loop_hash_rali_5_below = {}
     hash_rali_5_below = {}
     loop_hash_rali_5 = {}
@@ -413,11 +386,8 @@ class EmployeesController < ApplicationController
     hash_rali_30 = {}
     loop_hash_rali_35_up = {}
     hash_rali_35_up = {}
-    #生成【车间-人数】hash---结束
 
-    #使用循环把车间和人数存成hash
     @workshops.each do |i|
-      #把每个车间的总人数存成变量
       emp = Employee.where(workshop: i).count
       ca1 = Employee.where(workshop: i, rali_years: 0...5).count
       ca = (ca1.to_f)/(emp.to_f)
@@ -459,13 +429,8 @@ class EmployeesController < ApplicationController
       loop_hash_rali_35_up = {i => ch}
       hash_rali_35_up[i] = loop_hash_rali_35_up[i]
     end
-    #生成每个年龄段的【车间-人数】hash---结束
 
-    # 使用'gon'这个gem的方法来赋值变量，在js中使用
-    # 将以上算出最终hash的keys赋值给变量，作为条形图的坐标轴
     gon.bar_workshop = hash_rali_5_below.keys
-    #将以上算出最终hash的values赋值给对应的变量，作为条形图的数据
-    #路龄分析条形图
     gon.bar_rali_5_below = hash_rali_5_below.values
     gon.bar_rali_5 = hash_rali_5.values
     gon.bar_rali_10 = hash_rali_10.values
@@ -474,9 +439,8 @@ class EmployeesController < ApplicationController
     gon.bar_rali_25 = hash_rali_25.values
     gon.bar_rali_30 = hash_rali_30.values
     gon.bar_rali_35 = hash_rali_35_up.values
-    ## 条形图数据配置---结束
   end
-
+###年龄、工龄、路龄、学历图表分析页面数据配置---结束
 
 ###点击图表详情页面数据配置---开始
   def statistical_analysis_data
