67697870d509d9db01495659a919e2653a743889
kafeidebai <zhanggongzifei@163.com>
2018-05-09 13:46:40 +0800

    修改user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  rolify :before_add => :before_add_method
+  rolify :before_add => :before_add_method #增加角色时，要做些什么
   after_create :assign_default_role #创建用户后分配默认角色
 
   devise :database_authenticatable, :registerable,
@@ -7,6 +7,8 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true
 
+
+  # 增加角色前要做些什么
   def before_add_method(role)
 
   end
