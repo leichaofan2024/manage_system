a0caa73ec304e6a44d8cff233065d65c315d0a7d
kafeidebai <zhanggongzifei@163.com>
2018-05-10 15:21:42 +0800

    add db/seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-printf "创建多个管理员"
+puts "创建多个管理员"
 User.create(name: "超级管理员", password: "123456", password_confirmation: "123456")
 User.create(name: "现员管理员", password: "123456", password_confirmation: "123456")
 User.create(name: "考勤管理员", password: "123456", password_confirmation: "123456")
 User.create(name: "定额管理员", password: "123456", password_confirmation: "123456")
 User.create(name: "财务管理员", password: "123456", password_confirmation: "123456")
 User.create(name: "奖惩管理员", password: "123456", password_confirmation: "123456")
+puts "管理员创建成功"
+"\n"
 
-printf "创建成功"
+
+puts "创建车间管理员，请稍等。。。"
+Employee.pluck(:workshop).uniq.each do |e|
+  if e == "内退"
+    User.create(:name => "内退管理员", :password => "123456", :password_confirmation => "123456")
+  elsif e == "见习生"
+    User.create(:name => "见习生管理员", :password => "123456", :password_confirmation => "123456")
+  else
+    User.create(:name => e, :password => "123456", :password_confirmation => "123456")
+  end
+end
+workshop_count = Employee.pluck(:workshop).uniq.count
+puts "共创建#{workshop_count}个车间管理员"
+"\n"
+
+
+puts "创建班组管理员，请稍等。。。"
+Employee.pluck(:group).uniq.each do |e|
+  User.create(:name => e, :password => "123456", :password_confirmation => "123456")
+end
+group_count = Employee.pluck(:group).uniq.count
+puts "共创建#{group_count}个班组管理员"
