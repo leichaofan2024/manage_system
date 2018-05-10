6de46fbfb53659592720196b3999537415676de0
13949836783@163.com <13949836783@163.com>
2018-05-10 23:43:20 +0800

    初步完成弹窗更新，有bug

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,15 +2,27 @@ class AttendancesController < ApplicationController
 layout 'home'
 
 	def group
-		@employees = Employee.where(:id => 1..20)
+		@employees = Employee.where(:id => 1..12)
 	end
 
 	def create_attendance
-		
+		@attendance = Attendance.find_by(:employee_id => params[:employee_id])
+		a = @attendance.month_attendances.split('')
+		a[params[:day_number].to_i] = params[:code]
+		b = a.join('')
+		@attendance.update(:month_attendances => b)
+		@attendance.save
+		redirect_to group_attendances_path
 	end
 
 	def show_modal
-		@attendances = Attendance.first
+		@day_number = params[:day_number]
+		@categories = VacationCategory.all
+		@employee_id = params[:employee_id]
+		@vacation = {}
+		@categories.each do |category|
+			@vacation[category.vacation_shortening] = category.vacation_code
+		end
 		respond_to do |format|
 			format.js
 		end
