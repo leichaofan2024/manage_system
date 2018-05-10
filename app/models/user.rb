583322ef42a86e7295ae008332c8467f71a7bc77
kafeidebai <zhanggongzifei@163.com>
2018-05-10 17:02:07 +0800

    新建9个角色

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   rolify :before_add => :before_add_method #增加角色时，要做些什么
-  # after_create :assign_default_role #创建用户后分配默认角色
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
@@ -12,11 +11,6 @@ class User < ActiveRecord::Base
   def before_add_method(role)
 
   end
-
-  #分配默认角色
-  # def assign_default_role
-  #   self.add_role(:newuser) if self.roles.blank?
-  # end
 
 
 end
