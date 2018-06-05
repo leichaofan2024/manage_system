9cd9a1476b8e3c87a03decef6a2a093b272c11d4
kafeidebai <zhanggongzifei@163.com>
2018-06-05 19:40:12 +0800

    debug

--- db/migrate/20180521054237_rename_column_on_emp_basic_info.rb
+++
@@ -1,6 +0,0 @@
-class RenameColumnOnEmpBasicInfo < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :emp_basic_infos, :workshop, :workshop_id
-    rename_column :emp_basic_infos, :group, :group_id
-  end
-end
