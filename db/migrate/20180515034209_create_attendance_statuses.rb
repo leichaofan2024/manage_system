31472845693de94fc0cd4a570563205a382683a0
kafeidebai <zhanggongzifei@163.com>
2018-06-11 15:17:57 +0800

    add

--- db/migrate/20180515034209_create_attendance_statuses.rb
+++ db/migrate/20180515034209_create_attendance_statuses.rb
@@ -1,8 +1,8 @@
 class CreateAttendanceStatuses < ActiveRecord::Migration[5.1]
   def change
     create_table :attendance_statuses do |t|
-      t.integer :year, null: false
-      t.integer :month, null: false
+      t.integer :year
+      t.integer :month
       t.integer :group_id, null: false
       t.string :status, null: false
       t.timestamps
