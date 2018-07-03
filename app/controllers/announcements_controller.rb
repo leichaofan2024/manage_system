d6e4134346f103a54ac3d64fef8b78847f9e1663
kafeidebai <zhanggongzifei@163.com>
2018-07-03 13:54:32 +0800

    建立公告系统

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -4,6 +4,10 @@ class AnnouncementsController < ApplicationController
     @notices = Announcement.order('id DESC').page(params[:page]).per(20)
   end
 
+  def show
+    @notice = Announcement.find(params[:id])
+  end
+
   def new
     @notice = Announcement.new
   end
@@ -11,25 +15,39 @@ class AnnouncementsController < ApplicationController
   def create
     @notice = Announcement.new(notice_params)
     @notice.user = current_user
-    if  @notice.save!
+    if  @notice.save
       flash[:notice] = "新增关于#{@notice.title} 的公告"
       redirect_to announcements_path
     else
       flash[:alert] = "新增失败"
       render :new
     end
+
   end
 
   def edit
-
+    @notice = Announcement.find(params[:id])
   end
 
   def update
-
+    @notice = Announcement.find(params[:id])
+    if @notice.update(notice_params)
+      flash[:notice] = "更新成功"
+      redirect_to announcements_path
+    else
+      flash[:warning] = "更新失败"
+      render :edit
+    end
   end
 
-  def destory
-
+  def destroy
+    @notice = Announcement.find(params[:id])
+    if @notice.destroy
+      flash[:notice] = "已删除"
+      redirect_to announcements_path
+    else
+      flash[:warning] = "删除失败"
+    end
   end
 
   private
--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,3 +1,4 @@
 class Announcement < ApplicationRecord
   belongs_to :user, optional: true
+  validates_presence_of :title, :content
 end
