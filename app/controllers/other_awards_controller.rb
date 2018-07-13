21525513b20965285a9531c73f91cf411f027ec5
kafeidebai <zhanggongzifei@163.com>
2018-07-13 16:53:51 +0800

    改写其他返奖明细表

--- app/controllers/other_awards_controller.rb
+++ app/controllers/other_awards_controller.rb
@@ -8,11 +8,27 @@ class OtherAwardsController < ApplicationController
   def import
     if !params[:file].present?
       flash[:alert] = "您还没有选择文件哦"
+      render :new
     else
-      OtherAward.import(params[:file])
+      OtherAward.import(params[:file], params[:upload_time])
       flash[:notice] = "上传成功"
+      redirect_to other_awards_path
     end
-    redirect_to other_awards_path
+  end
+
+  def new
+    @other_award = OtherAward.new
+  end
+
+  def create
+    @other_award = OtherAward.new
+    if @other_award.save!
+      flash[:notice] = "上传成功"
+      redirect_to other_awards_path
+    else
+      flash[:notice] = "上传失败"
+      render :new
+    end
   end
 
 end
