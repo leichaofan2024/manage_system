a01d2f343e76aee37d3ac80acd8e71ef805f4613
kafeidebai <zhanggongzifei@163.com>
2018-07-13 16:42:14 +0800

    改写班组长返奖明细表

--- app/controllers/teamleader_awards_controller.rb
+++ app/controllers/teamleader_awards_controller.rb
@@ -9,10 +9,25 @@ class TeamleaderAwardsController < ApplicationController
     if !params[:file].present?
       flash[:alert] = "您还没有选择文件哦"
     else
-      TeamleaderAward.import(params[:file])
+      TeamleaderAward.import(params[:file], params[:upload_time])
       flash[:notice] = "上传成功"
     end
     redirect_to teamleader_awards_path
+  end
+
+  def new
+    @teamleader_award = TeamleaderAward.new
+  end
+
+  def create
+    @teamleader_award = TeamleaderAward.new
+    if @teamleader_award.save!
+      flash[:notice] = "上传成功"
+      redirect_to teamleader_awards_path
+    else
+      flash[:notice] = "上传失败"
+      render :new
+    end
   end
 
 
