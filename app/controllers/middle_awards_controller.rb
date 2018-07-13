0f25a2454ffdc864955b7d8dfb1ac5be6539a532
kafeidebai <zhanggongzifei@163.com>
2018-07-13 16:34:38 +0800

    改写中层返奖明细表

--- app/controllers/middle_awards_controller.rb
+++ app/controllers/middle_awards_controller.rb
@@ -8,11 +8,27 @@ class MiddleAwardsController < ApplicationController
   def import
     if !params[:file].present?
       flash[:alert] = "您还没有选择文件哦"
+      render :new
     else
-      MiddleAward.import(params[:file])
+      MiddleAward.import(params[:file], params[:upload_time])
       flash[:notice] = "上传成功"
+      redirect_to middle_awards_path
     end
-    redirect_to middle_awards_path
+  end
+
+  def new
+    @middle_award = MiddleAward.new
+  end
+
+  def create
+    @middle_award = MiddleAward.new
+    if @middle_award.save!
+      flash[:notice] = "上传成功"
+      redirect_to middle_awards_path
+    else
+      flash[:notice] = "上传失败"
+      render :new
+    end
   end
 
 end
