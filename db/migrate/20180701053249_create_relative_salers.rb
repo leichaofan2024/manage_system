ad640c737353b8e271c26de77d50fc321fb37bdc
kafeidebai <zhanggongzifei@163.com>
2018-07-16 14:18:23 +0800

    修改工挂工资明细表

--- db/migrate/20180701053249_create_relative_salers.rb
+++ db/migrate/20180701053249_create_relative_salers.rb
@@ -1,22 +1,22 @@
 class CreateRelativeSalers < ActiveRecord::Migration[5.1]
   def change
-    create_table :relative_salers, comment: "工效挂钩工资明细表" do |t|
+    create_table :relative_salers, comment: "工效挂钩工资明细表（科室车间上传）" do |t|
       t.string :序号
       t.string :科室车间
       t.string :部门班组
       t.string :工资号
       t.string :姓名
+      t.string :系数
       t.string :挂钩工资
       t.string :安全质量
       t.string :工作质量
-      t.string :行车
-      t.string :整改返奖
       t.string :一体化
       t.string :兼职兼岗
       t.string :其他
       t.string :应发
       t.string :考核扣款
       t.string :合计
+      t.string :备注
       t.integer :user_id
       t.timestamps
     end
