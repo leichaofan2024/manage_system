d7a47254356bf4aee75be6dfb6b26a5cc181ddeb
kafeidebai <zhanggongzifei@163.com>
2018-07-18 11:15:27 +0800

    debug leaderadmin

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -26,7 +26,7 @@ class EmployeesController < ApplicationController
   # end
 
   def index
-    if (current_user.has_role? :superadmin) || (current_user.has_role? :empadmin) || (current_user.has_role? :attendance_admin) || (current_user.has_role? :limitadmin) || (current_user.has_role? :awardadmin)
+    if  (current_user.has_role? :leaderadmin) || (current_user.has_role? :superadmin) || (current_user.has_role? :empadmin) || (current_user.has_role? :attendance_admin) || (current_user.has_role? :limitadmin) || (current_user.has_role? :awardadmin)
       @employees = Employee.current.order('id ASC').page(params[:page]).per(15)
     elsif current_user.has_role? :workshopadmin
       workshop_id = Workshop.find_by(:name => current_user.name).id
