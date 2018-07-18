58408465190a0907c54d7ea3d0714c35d1b74953
13949836783@163.com <13949836783@163.com>
2018-07-18 14:16:32 +0800

    删除未完成的功能

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -26,7 +26,7 @@ class EmployeesController < ApplicationController
   # end
 
   def index
-    if (current_user.has_role? :superadmin) || (current_user.has_role? :empadmin) || (current_user.has_role? :attendance_admin) || (current_user.has_role? :limitadmin) || (current_user.has_role? :awardadmin)
+    if (current_user.has_role? :leaderadmin) || (current_user.has_role? :superadmin) || (current_user.has_role? :empadmin) || (current_user.has_role? :attendance_admin) || (current_user.has_role? :limitadmin) || (current_user.has_role? :awardadmin)
       @employees = Employee.current.order('id ASC').page(params[:page]).per(15)
     elsif current_user.has_role? :workshopadmin
       workshop_id = Workshop.find_by(:name => current_user.name).id
@@ -1429,7 +1429,6 @@ class EmployeesController < ApplicationController
       @employees = LeavingEmployee.where(:leaving_type => "调离")
     elsif params[:type] == "调动"
       @employees = LeavingEmployee.where(:leaving_type => "调动")
-
     end
   end
 
