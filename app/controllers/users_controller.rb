2e9ad77186f52a4eb8fd9fdf9c1bdaefc9d0da41
kafeidebai <zhanggongzifei@163.com>
2018-07-18 15:34:36 +0800

    用户管理框架

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,22 +11,41 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
+    if @user.save
+      flash[:notice] = "新增#{@user.name} 成功"
+      redirect_to user_path(params[:id])
+    else
+      flash[:warning] = "新增失败"
+      render :new
+    end
   end
 
   def edit
-
+    @user = User.find(params[:id])
   end
 
   def update
-
+    @user = User.find(params[:id])
+    if @user.update(new_params)
+      flash[:notice] = "#{@user.name} 的信息更新成功"
+      redirect_to user_path(params[:id])
+    else
+      flash[:warngin] = "更新信息失败"
+      render :edit
+    end
   end
 
   def destroy
-
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = "#{@user.name} 已删除"
+    redirect_to users_path
   end
 
   private
-    
+
+    def user_params
+      params.require(:users).permit(:name, :password, :password_confirmation)
+    end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180716091645) do
+ActiveRecord::Schema.define(version: 20180718065527) do
 
   create_table "announcements", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string "title"
@@ -470,6 +470,7 @@ ActiveRecord::Schema.define(version: 20180716091645) do
     t.bigint "resource_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "translate_role"
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
     t.index ["name"], name: "index_roles_on_name"
     t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
