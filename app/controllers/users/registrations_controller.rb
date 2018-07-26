7953552483c29358cf91084147bd620021779f9a
kafeidebai <zhanggongzifei@163.com>
2018-07-26 15:41:23 +0800

    正常创建用户

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -19,14 +19,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
-    hash = { "车间管理员" => 'workshopadmin', "科室管理员" => 'organsadmin', "班组管理员" => 'groupadmin', "领导" => 'leaderadmin' }
+    hash = { "optionID2" => 'workshopadmin', "optionID3" => 'organsadmin', "optionID1" => 'groupadmin', "optionID4" => 'leaderadmin' }
     @user = User.new(:name => params[:user][:name],
                      :password => params[:user][:password],
                      :password_confirmation => params[:user][:password_confirmation],
-                     :workshop_id => params[:user][:workshop],
-                     :group_id => params[:user][:group])
-    @user.add_role(hash[params[:user][:roles]])
-    binding.pry
+                     :workshop_id => params[:workshop],
+                     :group_id => params[:group])
+    @user.add_role(hash[params[:roles]])
     if @user.save
       flash[:notice] = "新增#{@user.name} 成功"
       redirect_to users_path
