941419883a5f8b6ecb05ebcc06f4afb09970b596
kafeidebai <zhanggongzifei@163.com>
2018-07-31 13:43:18 +0800

    添加用户名唯一

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable
 
   validates :name, presence: { message: "请填写姓名"}
+  validates :name, uniqueness: { message: "已有其他用户使用该用户名"}
   validates :password, presence: { message: "请设置密码，最小长度为6位数字"}
   validates :password_confirmation, presence: { message: "密码不一致"}
   has_many :messages
