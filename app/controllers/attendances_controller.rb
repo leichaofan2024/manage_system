492e3346c0d06f06ab0f22e6c42604420bdb785b
13949836783@163.com <13949836783@163.com>
2018-08-01 09:13:11 +0800

    修改代码，新增当月考勤

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -33,11 +33,11 @@ layout 'home'
 					attendance_status.update(group_id: Employee.find(i).group, status: "班组/科室填写中")
 				end
 			else
-				attendance = Attendance.where(employee_id: i, year: Time.now.year, month: Time.now.month + 1)
+				attendance = Attendance.where(employee_id: i, year: Time.now.year, month: Time.now.month)
 				if !attendance.present?
-					Attendance.create(employee_id: i, group_id: Employee.find(i).group, year: Time.now.year, month: Time.now.month + 1)
-					attendance_status = AttendanceStatus.find_by(group_id: Employee.find(i).group) || AttendanceStatus.new
-					attendance_status.update(group_id: Employee.find(i).group, status: "班组/科室填写中")
+					Attendance.create(employee_id: i, group_id: Employee.find(i).group, year: Time.now.year, month: Time.now.month)
+					# attendance_status = AttendanceStatus.find_by(group_id: Employee.find(i).group) || AttendanceStatus.new
+					# attendance_status.update(group_id: Employee.find(i).group, status: "班组/科室填写中")
 				end
 			end
 			flash[:notice] = "下月考勤表新增成功"
