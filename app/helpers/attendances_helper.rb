9b98fa8a5eeeece30afbb38680a348a13bc0fd1c
V蛋 <610550052@qq.com>
2018-09-17 13:35:33 +0800

    将状态栏的位置调整为标题附近

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,2 +1,19 @@
 module AttendancesHelper
+  def workshop_checked(shenhe_day,groups,shenhe_year,shenhe_month,status)
+    if shenhe_day === Time.now.day
+      if AttendanceStatus.where(:group_id => groups.ids,:year => shenhe_year, :month => shenhe_month,:status => status).present?
+        "有班组待审核"
+      else
+        "暂时没有可以审核的班组"
+      end
+    end
+  end
+
+  def groups_checked(shenhe_day,group,year,month)
+    if shenhe_day === Time.now.day
+        if AttendanceStatus.where(group_id: group, year: year, month: month).present?
+          AttendanceStatus.find_by(group_id: group, year: year, month: month).status
+        end
+    end
+  end
 end
