7ea2bdd3c2a8be7861750fd223797150cb556c0d
V蛋 <610550052@qq.com>
2018-09-17 19:36:49 +0800

    标题状态不显示的bug已经解决

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,19 +1,15 @@
 module AttendancesHelper
   def workshop_checked(shenhe_day,groups,shenhe_year,shenhe_month,status)
-    if shenhe_day === Time.now.day
-      if AttendanceStatus.where(:group_id => groups.ids,:year => shenhe_year, :month => shenhe_month,:status => status).present?
-        "有班组待审核"
-      else
-        "暂时没有可以审核的班组"
-      end
+    if AttendanceStatus.where(:group_id => groups.ids,:year => shenhe_year, :month => shenhe_month,:status => status).present? && shenhe_day === Time.now.day
+      "有班组待审核"
+    else
+      "暂时没有可以审核的班组"
     end
   end
 
-  def groups_checked(shenhe_day,group,year,month)
-    if shenhe_day === Time.now.day
-        if AttendanceStatus.where(group_id: group, year: year, month: month).present?
-          AttendanceStatus.find_by(group_id: group, year: year, month: month).status
-        end
+  def groups_checked(group,year,month)
+    if AttendanceStatus.where(group_id: group, year: year, month: month).present?
+      AttendanceStatus.find_by(group_id: group, year: year, month: month).status
     end
   end
 end
