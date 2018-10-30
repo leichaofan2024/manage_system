a278d19e4feae042feb32a6b7820cf844d09e088
雷超凡 <leichaofan1987@163.com>
2018-10-30 15:04:30 +0800

    完成其他返奖功能

--- app/controllers/other_awards_controller.rb
+++ app/controllers/other_awards_controller.rb
@@ -1,34 +1,65 @@
 class OtherAwardsController < ApplicationController
   layout 'home'
 
+
   def index
-    @other_awards = OtherAward.page(params[:page]).per(15)
+    if params[:year].present?
+      @year = params[:year].to_i
+      @month = Array.new
+      if params[:month].present?
+        params[:month].each do |month|
+          @month << month.to_i
+        end
+      else
+        @month = OtherAward.where(:upload_year => params[:year]).pluck(:upload_month).map{|x| x.to_i}.uniq
+      end
+    else
+      @year = Time.now.year
+      @month = [Time.now.month]
+    end
+    @years = OtherAward.pluck(:upload_year).map{|x| x.to_i}.uniq
+    @months = OtherAward.pluck(:upload_month).map{|x| x.to_i}.uniq
+    @other_awards = OtherAward.all.page(params[:page]).per(20)
+    @export_other_awards = OtherAward.all
+    respond_to do |format|
+      format.html
+      format.csv { send_data @export_other_awards.to_csv}
+      format.xls { headers["Content-Disposition"] = 'attachment; filename="其他返奖明细表.xls"'}
+    end
   end
 
   def import
+
     if !params[:file].present?
       flash[:alert] = "您还没有选择文件哦"
-      render :new
+    elsif !params[:upload_time].present?
+      flash[:alert] = "您还没有选择日期哦"
     else
-      OtherAward.import(params[:file], params[:upload_time])
-      flash[:notice] = "上传成功"
-      redirect_to other_awards_path
+      @import_message = OtherAward.import_form(params[:file],params[:upload_time])
+      if @import_message[:head].present?
+        flash[:alert] = @import_message[:head]
+      elsif @import_message[:year_month].present?
+        flash[:alert] = @import_message[:year_month]
+      else
+        flash[:notice] = "上传成功"
+      end
+    end
+    redirect_to other_awards_path
+  end
+
+
+  def show_modal
+    respond_to do |format|
+      format.js
     end
   end
 
-  def new
-    @other_award = OtherAward.new
-  end
-
-  def create
-    @other_award = OtherAward.new
-    if @other_award.save!
-      flash[:notice] = "上传成功"
-      redirect_to other_awards_path
-    else
-      flash[:notice] = "上传失败"
-      render :new
-    end
+  def delete_other_awards
+    @year = params[:year]
+    @month = params[:month]
+    OtherAward.where(:upload_year => @year,:upload_month => @month).delete_all
+    flash[:notice] = "#{@year}年#{@month}月其他返奖明细表已成功删除！"
+    redirect_to other_awards_path
   end
 
 end
