b28853f69f0d0435ab2638ed2a304e5f8f298fc6
雷超凡 <leichaofan1987@163.com>
2018-10-30 15:18:58 +0800

    抽考扣款明细表功能完成

--- app/controllers/examination_charges_controller.rb
+++ app/controllers/examination_charges_controller.rb
@@ -1,33 +1,65 @@
 class ExaminationChargesController < ApplicationController
  layout 'home'
 
+
   def index
-    @examination_charges = ExaminationCharge.page(params[:page]).per(15)
+    if params[:year].present?
+      @year = params[:year].to_i
+      @month = Array.new
+      if params[:month].present?
+        params[:month].each do |month|
+          @month << month.to_i
+        end
+      else
+        @month = ExaminationCharge.where(:upload_year => params[:year]).pluck(:upload_month).map{|x| x.to_i}.uniq
+      end
+    else
+      @year = Time.now.year
+      @month = [Time.now.month]
+    end
+    @years = ExaminationCharge.pluck(:upload_year).map{|x| x.to_i}.uniq
+    @months = ExaminationCharge.pluck(:upload_month).map{|x| x.to_i}.uniq
+    @examination_charges = ExaminationCharge.all.page(params[:page]).per(20)
+    @export_examination_charges = ExaminationCharge.all
+    respond_to do |format|
+      format.html
+      format.csv { send_data @export_examination_charges.to_csv}
+      format.xls { headers["Content-Disposition"] = 'attachment; filename="抽考扣款明细表.xls"'}
+    end
   end
 
   def import
+
     if !params[:file].present?
       flash[:alert] = "您还没有选择文件哦"
+    elsif !params[:upload_time].present?
+      flash[:alert] = "您还没有选择日期哦"
     else
-      ExaminationCharge.import(params[:file], params[:upload_time])
-      flash[:notice] = "上传成功"
+      @import_message = ExaminationCharge.import_form(params[:file],params[:upload_time])
+      if @import_message[:head].present?
+        flash[:alert] = @import_message[:head]
+      elsif @import_message[:year_month].present?
+        flash[:alert] = @import_message[:year_month]
+      else
+        flash[:notice] = "上传成功"
+      end
     end
     redirect_to examination_charges_path
   end
 
-  def new
-    @examination_charge = ExaminationCharge.new
+
+  def show_modal
+    respond_to do |format|
+      format.js
+    end
   end
 
-  def create
-    @examination_charge = ExaminationCharge.new
-    if @examination_charge.save!
-      flash[:notice] = "上传成功"
-      redirect_to examination_charges_path
-    else
-      flash[:notice] = "上传失败"
-      render :new
-    end
+  def delete_examination_charges
+    @year = params[:year]
+    @month = params[:month]
+    ExaminationCharge.where(:upload_year => @year,:upload_month => @month).delete_all
+    flash[:notice] = "#{@year}年#{@month}月抽考扣款明细表已成功删除！"
+    redirect_to examination_charges_path
   end
 
 end
--- app/models/examination_charge.rb
+++ app/models/examination_charge.rb
@@ -1,16 +1,33 @@
 class ExaminationCharge < ApplicationRecord
 
-  def self.import(file, upload_time)
+  def self.import_form(file, upload_time)
+
+    message = Hash.new
     spreadsheet = Roo::Spreadsheet.open(file.path)
-    header = spreadsheet.row(1)
-    (2..spreadsheet.last_row).each do |i|
-        row = Hash[[header, spreadsheet.row(i)].transpose]
-        saler = find_by(id: row["id"]) || new
-        saler.attributes = row.to_hash
-        saler.upload_year = upload_time.split("-")[0]
-        saler.upload_month = upload_time.split("-")[1]
-        saler.save!
+    import_form_headers = spreadsheet.row(1)
+    form_headers = ExaminationCharge.column_names
+    import_form_headers.each do |fh|
+      if !form_headers.include?(fh)
+        message[:head] = "所上传的抽考扣款明细表表头为‘#{fh}’的字段与系统不匹配，请核对后再上传！"
+      end
     end
+    year_month_already_import = ExaminationCharge.pluck(:upload_year,:upload_month).uniq
+    year = upload_time.split("-")[0].to_s
+    month = upload_time.split("-")[1].to_s
+    if year_month_already_import.include?([year,month])
+      message[:year_month] = "本月数据已上传，请勿重复上传！"
+    end
+
+    if message.blank?
+      (2..spreadsheet.last_row).each do |n|
+        row_hash = [spreadsheet.row(1),spreadsheet.row(n)].transpose.to_h
+        row_hash[:upload_year] = year
+        row_hash[:upload_month] = month
+        ExaminationCharge.create(row_hash)
+
+      end
+    end
+    return message
   end
 
 end
