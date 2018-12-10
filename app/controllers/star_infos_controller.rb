0fb78a34830a41750ecf287526d480a8de70d6ee
13949836783@163.com <13949836783@163.com>
2018-12-10 15:21:34 +0800

    修改按钮样式

--- app/controllers/star_infos_controller.rb
+++ app/controllers/star_infos_controller.rb
@@ -17,12 +17,11 @@ class StarInfosController < ApplicationController
 		updated_star = (star.star).to_i + (params[:update_type] + params[:star_count]).to_i
 		if (updated_star > 5) || (updated_star < 1)
 			flash[:alert] = "升/降星后该人员的星级高于五星或低于一星，请检查"
-			render "star_infos/all_star_info"
 		else
 			star.update(star: updated_star)
-			flash[:notice] = "更新完成"
-			redirect_to all_star_info_star_infos_path
+			flash[:notice] = "更新完成"	
 		end	
+		redirect_to all_star_info_star_infos_path
 	end
 end
 end
