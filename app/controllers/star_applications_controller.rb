7353e2a367b05c4c32639791ef73b1f92efbb469
13949836783@163.com <13949836783@163.com>
2018-12-12 14:58:53 +0800

    车间管理员可发起申请

--- app/controllers/star_applications_controller.rb
+++ app/controllers/star_applications_controller.rb
@@ -2,17 +2,17 @@ class StarApplicationsController < ApplicationController
 	layout 'home'
 
 	def index
-		if params[:type] == "0"
-			@applications = StarApplication.where(status: 0)
-		elsif params[:type] == "1"
+		if params[:type] == "1"
 			@applications = StarApplication.where(status: 1)
+		else
+			@applications = StarApplication.where(status: 0)
 		end
 	end
 
 	def refuse_application
 		StarApplication.find(params[:application]).update(status: 1, treatment_state: "拒绝")
 		flash[:notice] = "已拒绝本次申请，流程结束"
-		redirect_to star_applications_path
+		redirect_to star_applications_path(type: 0)
 	end
 
 	def agree_application
@@ -25,10 +25,23 @@ class StarApplicationsController < ApplicationController
 			StarInfo.find(application.star_info_id).update(star: updated_star)
 			flash[:notice] = "您已通过本次申请，系统已自动更新该人员的星级"
 		end
-		redirect_to star_applications_path
+		redirect_to star_applications_path(type: 0)
 	end
 
 	def create_application
-		
+		up_down_star = (params[:update_type] + params[:star_count]).to_i
+		 
+		if StarInfo.find_by(name: params[:name], sal_number: params[:sal_number], year: params[:year], quarter: params[:quarter]).present?
+			star_info = StarInfo.find_by(name: params[:name], sal_number: params[:sal_number], year: params[:year], quarter: params[:quarter])
+			if (((star_info.star).to_i + up_down_star).to_i > 0) || (((star_info.star).to_i + up_down_star).to_i < 6)
+				StarApplication.create(star_info_id: star_info.id, up_down_star: up_down_star, applicant: current_user.name)
+				flash[:notice] = "成功发起申请"
+			else
+				flash[:alert] = "通过申请后该人员的星级将低于一星或高于五星，请检查"
+			end
+		else
+			flash[:alert] = "您填写的工资号或姓名不在系统内，请先上传"
+		end
+		redirect_to new_star_application_path
 	end
 end
