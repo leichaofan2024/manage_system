f7f349f260c527bbb6de331a918a0485d2bcc448
13949836783@163.com <13949836783@163.com>
2018-12-13 17:22:38 +0800

    debug

--- app/controllers/star_applications_controller.rb
+++ app/controllers/star_applications_controller.rb
@@ -10,20 +10,31 @@ class StarApplicationsController < ApplicationController
 	end
 
 	def refuse_application
-		StarApplication.find(params[:application]).update(status: 1, treatment_state: "拒绝")
-		flash[:notice] = "已拒绝本次申请，流程结束"
+		application = StarApplication.find(params[:application])
+		basic_score = BasicScore.find(StarInfo.find(application.star_info_id).basic_score_id)
+		if basic_score.confirm_status == 1
+			StarApplication.find(params[:application]).update(status: 1, treatment_state: "拒绝")
+			flash[:notice] = "已拒绝本次申请，流程结束"
+		else
+			flash[:alert] = "您还未完成本季度的评定，请完成后再处理申请"
+		end
 		redirect_to star_applications_path(type: 0)
 	end
 
 	def agree_application
 		application = StarApplication.find(params[:application])
 		updated_star = (StarInfo.find(application.star_info_id).star).to_i + application.up_down_star	
-		if (updated_star > 5) || (updated_star < 1)
-			flash[:alert] = "升/降星后该人员的星级高于五星或低于一星，请检查"
+		basic_score = BasicScore.find(StarInfo.find(application.star_info_id).basic_score_id)
+		if basic_score.confirm_status == 1
+			if (updated_star > 5) || (updated_star < 1)
+				flash[:alert] = "升/降星后该人员的星级高于五星或低于一星，请检查"
+			else
+				application.update(status: 1, treatment_state: "通过")
+				StarInfo.find(application.star_info_id).update(star: updated_star)
+				flash[:notice] = "您已通过本次申请，系统已自动更新该人员的星级"
+			end
 		else
-			application.update(status: 1, treatment_state: "通过")
-			StarInfo.find(application.star_info_id).update(star: updated_star)
-			flash[:notice] = "您已通过本次申请，系统已自动更新该人员的星级"
+			flash[:alert] = "您还未完成本季度的评定，请完成后再处理申请"
 		end
 		redirect_to star_applications_path(type: 0)
 	end
@@ -33,10 +44,11 @@ class StarApplicationsController < ApplicationController
 		 
 		if StarInfo.find_by(name: params[:name], sal_number: params[:sal_number], year: params[:year], quarter: params[:quarter]).present?
 			star_info = StarInfo.find_by(name: params[:name], sal_number: params[:sal_number], year: params[:year], quarter: params[:quarter])
-			if (((star_info.star).to_i + up_down_star).to_i > 0) || (((star_info.star).to_i + up_down_star).to_i < 6)
+			if (((star_info.star).to_i + up_down_star).to_i > 0) && (((star_info.star).to_i + up_down_star).to_i < 6)
 				StarApplication.create(star_info_id: star_info.id, up_down_star: up_down_star, applicant: current_user.name)
 				flash[:notice] = "成功发起申请"
 			else
+				
 				flash[:alert] = "通过申请后该人员的星级将低于一星或高于五星，请检查"
 			end
 		else
--- app/controllers/star_infos_controller.rb
+++ app/controllers/star_infos_controller.rb
@@ -14,27 +14,35 @@ class StarInfosController < ApplicationController
 
 		@year = params[:year]
 		@quarter = params[:quarter]
-		@type = params[:type]
 		quarter_month = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
 		quarter_month.each do |i|
 			if i.include?(Time.now.month)
 				@current_quarter = (quarter_month.index(i) + 1)
 			end
 		end
-		if (@year.present?) || (@quarter.present?)
-			if @type == "passed"
-				@scores = BasicScore.where(confirm_status: 1, year: @year, quarter: @quarter)
-			else
-				@scores = BasicScore.where(year: @year, quarter: @quarter)
-			end
+		if params[:type] == "1"
+			@type = 1
 		else
-			if @type == "passed"
-				@scores = BasicScore.where(confirm_status: 1, year: Time.now.year, quarter: @current_quarter)
+			@type = [0, 1]
+		end
+		if params[:year].present?
+			@year = params[:year]
+			@quarter = params[:quarter]
+		else
+			@year = Time.now.year
+			@quarter = @current_quarter
+		end
+		if params[:basic_score_ids].present?
+			@scores = BasicScore.where(id: params[:basic_score_ids], confirm_status: @type, year: @year, quarter: @quarter)
+		else
+			if params[:condition].present?
+				@scores = []
+				flash[:alert] = "未找到符合条件的人员"
 			else
-				@scores = BasicScore.where(year: Time.now.year, quarter: @current_quarter)
+				@scores = BasicScore.where(confirm_status: @type, year: @year, quarter: @quarter)
 			end
 		end
-		@export_scores = @scores
+		@export_scores = BasicScore.where(id: params[:export_scores])
 		respond_to do |format|
 	      format.html
 	      format.js
@@ -75,6 +83,7 @@ class StarInfosController < ApplicationController
 		if params[:group] == "--选择班组--"
 			params[:group] = nil
 		end
+		
 		# 配置车间班组筛选框数据
 		workshop = StarInfo.pluck("workshop").uniq
 		@group = [["--选择班组--"]]
@@ -85,18 +94,22 @@ class StarInfosController < ApplicationController
 		@workshop_names = StarInfo.pluck("workshop").uniq
 		@year = params[:year]
 		@quarter = params[:quarter]
-		@type = params[:type]
+		@params_workshop = params[:workshop]
+		@params_group = params[:group]
 		quarter_month = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
 		quarter_month.each do |i|
 			if i.include?(Time.now.month)
 				@current_quarter = (quarter_month.index(i) + 1)
 			end
 		end
-		if (@year.present?) || (@quarter.present?)
-			condition = ".where(year: @year, quarter: @quarter"
+		if params[:year].present?
+			@year = params[:year]
+			@quarter = params[:quarter]
 		else
-			condition = ".where(year: Time.now.year, quarter: @current_quarter"
+			@year = Time.now.year
+			@quarter = @current_quarter
 		end
+		condition = ".where(year: #{@year}, quarter: #{@quarter}"
 	    if params[:workshop].present?
 	      condition += ", workshop: '#{params[:workshop]}'"
 	    end
@@ -110,11 +123,55 @@ class StarInfosController < ApplicationController
 	      condition += ", star: '#{params[:star]}'"
 	    end
 	    condition += ")"
-	    @scores = eval("StarInfo#{condition}")
-	    binding.pry
-	    @export_scores = @scores
-		render :action => "all_star_info"
+	    basic_score_ids = eval("StarInfo#{condition}").pluck(:basic_score_id)
+		redirect_to :action => "all_star_info", basic_score_ids: basic_score_ids, condition: condition, year: params[:year], quarter: params[:quarter]
 	end
+
+	# def filter
+	# 	if params[:workshop] == "--选择车间--"
+	# 		params[:workshop] = nil
+	# 	end
+	# 	if params[:group] == "--选择班组--"
+	# 		params[:group] = nil
+	# 	end
+	# 	# 配置车间班组筛选框数据
+	# 	workshop = StarInfo.pluck("workshop").uniq
+	# 	@group = [["--选择班组--"]]
+	#     workshop.each do |name|
+	#       @group << StarInfo.where(workshop: name).pluck(:group).uniq
+	#     end
+	#     gon.group_name = @group
+	# 	@workshop_names = StarInfo.pluck("workshop").uniq
+	# 	@year = params[:year]
+	# 	@quarter = params[:quarter]
+	# 	@type = params[:type]
+	# 	quarter_month = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+	# 	quarter_month.each do |i|
+	# 		if i.include?(Time.now.month)
+	# 			@current_quarter = (quarter_month.index(i) + 1)
+	# 		end
+	# 	end
+	# 	if (@year.present?) || (@quarter.present?)
+	# 		condition = ".where(year: @year, quarter: @quarter"
+	# 	else
+	# 		condition = ".where(year: Time.now.year, quarter: @current_quarter"
+	# 	end
+	#     if params[:workshop].present?
+	#       condition += ", workshop: '#{params[:workshop]}'"
+	#     end
+	#     if params[:group].present?
+	#       condition += ", group: '#{params[:group]}'"
+	#     end
+	#     if params[:duty].present?
+	#       condition += ", duty: '#{params[:duty]}'"
+	#     end
+	#     if params[:star].present?
+	#       condition += ", star: '#{params[:star]}'"
+	#     end
+	#     condition += ")"
+	#     @scores = eval("StarInfo#{condition}")
+	# 	render :action => "all_star_info"
+	# end
 
 	protected
 
