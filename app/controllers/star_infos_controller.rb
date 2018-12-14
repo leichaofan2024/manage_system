9a19b1df65b1a7a538c568a199881e8d4600701a
雷超凡 <leichaofan1987@163.com>
2018-12-14 17:58:00 +0800

    五星岗位人员页

--- app/controllers/star_infos_controller.rb
+++ app/controllers/star_infos_controller.rb
@@ -132,51 +132,64 @@ class StarInfosController < ApplicationController
 		redirect_to :action => "all_star_info", basic_score_ids: basic_score_ids, condition: condition, year: params[:year], quarter: params[:quarter]
 	end
 
-	# def filter
-	# 	if params[:workshop] == "--选择车间--"
-	# 		params[:workshop] = nil
-	# 	end
-	# 	if params[:group] == "--选择班组--"
-	# 		params[:group] = nil
-	# 	end
-	# 	# 配置车间班组筛选框数据
-	# 	workshop = StarInfo.pluck("workshop").uniq
-	# 	@group = [["--选择班组--"]]
-	#     workshop.each do |name|
-	#       @group << StarInfo.where(workshop: name).pluck(:group).uniq
-	#     end
-	#     gon.group_name = @group
-	# 	@workshop_names = StarInfo.pluck("workshop").uniq
-	# 	@year = params[:year]
-	# 	@quarter = params[:quarter]
-	# 	@type = params[:type]
-	# 	quarter_month = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
-	# 	quarter_month.each do |i|
-	# 		if i.include?(Time.now.month)
-	# 			@current_quarter = (quarter_month.index(i) + 1)
-	# 		end
-	# 	end
-	# 	if (@year.present?) || (@quarter.present?)
-	# 		condition = ".where(year: @year, quarter: @quarter"
-	# 	else
-	# 		condition = ".where(year: Time.now.year, quarter: @current_quarter"
-	# 	end
-	#     if params[:workshop].present?
-	#       condition += ", workshop: '#{params[:workshop]}'"
-	#     end
-	#     if params[:group].present?
-	#       condition += ", group: '#{params[:group]}'"
-	#     end
-	#     if params[:duty].present?
-	#       condition += ", duty: '#{params[:duty]}'"
-	#     end
-	#     if params[:star].present?
-	#       condition += ", star: '#{params[:star]}'"
-	#     end
-	#     condition += ")"
-	#     @scores = eval("StarInfo#{condition}")
-	# 	render :action => "all_star_info"
-	# end
+    def five_star_info
+      @years = BasicScore.pluck(:year).uniq.sort{|a,b| b<=>a}
+	  @quarters = BasicScore.pluck(:quarter).uniq.sort{|a,b| b<=> a}
+	  quarter_hash = {1=>1,2=>1,3=>1,4=>2,5=>2,6=>2,7=>3,8=>3,9=>3,10=>4,11=>4,12=>4}
+	  if params[:year].present? 
+	  	@year = params[:year].to_i 
+	  else 
+	  	@year = Time.now.year 
+	  end 
+	  if params[:quarter].present? 
+	    @quarter = params[:quarter].to_i 
+	  else 
+        quarters = BasicScore.where(:year => Time.now.year).pluck(:quarter).uniq.sort{|a,b| b<=> a}
+        if quarters.present? 
+        	@quarter = quarters[0]
+        else 
+            @quarter = quarter_hash[Time.now.month]
+        end 
+	  end 
+	  if params[:passed_five_star].present? 
+	  	@star_infos = StarInfo.where(:star => "5")
+	  else 
+	  	@star_infos = StarInfo.where(:star => ["5","pre_5"])
+	  end 
+	  if current_user.has_role? :workshopadmin
+	  	@star_infos = @star_infos.where(:workshop => Workshop.find_by(:id =>current_user.workshop_id).name,:year => @year,:quarter => @quarter)
+	  	@groups = @star_infos.pluck(:group).uniq
+	  	@dutys = @star_infos.pluck(:duty).uniq
+	  elsif (current_user.has_role? :staradmin)  || (current_user.has_role? :superadmin) || (current_user.has_role? :leaderadmin) || (current_user.has_role? :depudy_leaderadmin)
+        @star_infos = @star_infos.where(:year => @year,:quarter => @quarter)
+        @workshops = @star_infos.pluck(:workshop).uniq
+        groups_array = [[]]
+        @workshops.each do |workshop|
+        	groups = @star_infos.where(:workshop => workshop).pluck(:group).uniq
+        	groups_array << groups 
+        end 
+        @dutys = @star_infos.pluck(:duty).uniq
+      end 
+      gon.groups = groups_array
+      if params[:workshop].present?
+      	@star_infos = @star_infos.where(:workshop => params[:workshop])
+      end 
+      if params[:group].present?
+      	@star_infos = @star_infos.where(:group => params[:group])
+      end
+      if params[:duty].present?
+      	@star_infos = @star_infos.where(:duty => params[:duty])
+      end
+  
+      @star_infos_export = @star_infos
+      @star_infos = @star_infos.page(params[:page]).per(20)
+      
+	  respond_to do |format| 
+	  	format.html
+	  	format.xls { headers["Content-Disposition"] = 'attachment; filename="五星级岗位表.xls"'}
+
+	  end  
+    end 
 
 	protected
 
